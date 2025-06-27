519f4208defc4697de6210d6c4316ebf01e9e335
Genz Alok <genzalok24@gmail.com>
2025-06-27 11:49:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "YourChannelUsername")
-DUMP_CHANNEL_ID = int(os.getenv("DUMP_CHANNEL_ID", "123456789"))
+BOT_TOKEN = os.getenv("7604228341:AAHjVUQewPI--nUvuM3WYlPFsHUv-xxAzK8")
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "filmyrip_official")
+DUMP_CHANNEL_ID = int(os.getenv("DUMP_CHANNEL_ID", "-1002297551055"))
